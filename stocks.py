b530abb2a7b07c88b133491ee811b19b31a97d10
Asaf Ben Dror <123545280+Asaf01@users.noreply.github.com>
2025-05-19 00:18:53 +0300

    Create stocks.py

---
+++ stocks.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from datetime import datetime, timedelta
+
+ticker = "nvda.us"                     #  U.S. stocks
+end   = datetime.today()
+start = end - timedelta(days=5*365)
+
+url = f"https://stooq.com/q/d/l/?s={ticker}&d1={start:%Y%m%d}&d2={end:%Y%m%d}&i=d"
+nvda = (pd.read_csv(url, parse_dates=["Date"])
+           .set_index("Date")
+           .sort_index())               # chronological order
+nvda
+
+
+#pip install panda 
